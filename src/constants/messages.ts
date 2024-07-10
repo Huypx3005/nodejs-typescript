94795227850542368cf615f983610e953c960771
Huypx3005 <Phamxuanhuy3005@gmail.com>
2024-07-10 18:01:02 +0700

    add error handling

---
+++ src/constants/messages.ts
@@ -0,0 +1,10 @@
+export const USERS_MESSAGES = {
+  VALIDATION_ERROR: 'Validation error',
+  NAME_IS_REQUIRED: 'Name is required',
+
+  EMAIL_IS_REQUIRED: 'Email is required',
+  EMAIL_IS_INVALID: 'Email is invalid',
+  PASSWORD_IS_REQUIRED: 'Password is required',
+  PASSWORD_IS_TOO_SHORT: 'Password must be at least 8 characters long',
+  PASSWORD_CONTAINS_AT_LEAST_ONE_UPPERCASE: 'Password must contain at least one uppercase letter'
+} as const
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import usersRouter from './routes/users.route'
 import databaseService from './services/database.services'
 
@@ -7,6 +7,13 @@ const port = 3000
 app.use(express.json())
 app.use('/users', usersRouter)
 databaseService.connect()
+
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+  console.log(error)
+  res.status(500).json({
+    error: 'Internal Server Error'
+  })
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
